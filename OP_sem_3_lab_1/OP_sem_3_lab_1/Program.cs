8dc353ec8d72946ea5417a76268af2f80f79d29b
Матвієнко Артем <artemm3notegp@gmail.com>
2023-10-08 17:16:06 +0300

    Project initialization with adding gitignore

---
+++ OP_sem_3_lab_1/OP_sem_3_lab_1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OP_sem_3_lab_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
