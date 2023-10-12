ee1b21f5f85dcddb119b12637bc77ddd58702446
Матвієнко Артем <artemm3notegp@gmail.com>
2023-10-12 18:05:54 +0300

    checkpoint before replace amount variables on static ones

--- OP_sem_3_lab_1/OP_sem_3_lab_1/Program.cs
+++ OP_sem_3_lab_1/OP_sem_3_lab_1/Program.cs
@@ -5,10 +5,16 @@
         static void Main(string[] args)
         {
             Animals animal = new Animals();
+            Console.WriteLine(animal);
 
             WaterAnimal waterAnimal = new WaterAnimal();
+            OverLand overlandAnimal = new OverLand();
 
             Console.WriteLine(waterAnimal.GetPercentOfAmount());
+            Console.WriteLine(waterAnimal.Habitat);
+            Console.WriteLine(overlandAnimal.GetPercentOfAmount());
+            Animals.x = 10;
+            Console.WriteLine(Animals.x);
         }
     }
 
@@ -28,17 +34,18 @@
 
     class Animals
     {
-        private string _habitat;
-        protected int _totalAmount;
+        public static int x = 999999;
+        private string habitat;
+        protected int totalAmount;
 
         public string Habitat
         {
-            get { return _habitat; }
+            get { return habitat; }
         }
 
         public int Amount
         {
-            get { return _totalAmount; }
+            get { return totalAmount; }
             set
             {
                 if (value < 0)
@@ -47,21 +54,21 @@
                 }
                 else
                 {
-                    _totalAmount = value;
+                    totalAmount = value;
                 }
             }
         }
 
         public Animals()
         {
-            _habitat = "Earth";
-            _totalAmount = 2000000000;
+            habitat = "Earth";
+            totalAmount = 2000000000;
         }
 
         public Animals(int amount)
         {
-            _habitat = "Earth";
-            _totalAmount = amount;
+            habitat = "Earth";
+            totalAmount = amount;
         }
 
         public virtual string Voice()
@@ -72,13 +79,18 @@
 
     class WaterAnimal : Animals
     {
-        private string _habitat;
-        private int _amount;
+        private string habitat;
+        protected int amountOfWaterAnimals;
+
+        public string Habitat
+        {
+            get => habitat;
+        }
 
         public WaterAnimal()
         {
-            _habitat = "Water";
-            _amount = 1500000000;
+            habitat = "Water";
+            amountOfWaterAnimals = 1500000000;
         }
 
         public override string Voice()
@@ -86,34 +98,51 @@
             return "I am water animal";
         }
 
-        public double GetPercentOfAmount()
+        public virtual double GetPercentOfAmount()
         {
-            Console.WriteLine("A: {0},    T: {1}", _amount, _totalAmount);
-            return ((double)_amount / (double)_totalAmount) * 100;
+            Console.WriteLine("A: {0};\tT: {1}", amountOfWaterAnimals, totalAmount);
+            return ((double)amountOfWaterAnimals / (double)totalAmount) * 100;
         }
     }
 
     class Fish : WaterAnimal
     {
-        private string _habitat;
+        private string habitat;
+        private int amount;
+
+        public string Habitat
+        {
+            get => habitat;
+        }
 
         public Fish()
         {
-            _habitat = "All ponds";
+            habitat = "All ponds";
         }
         public override string Voice()
         {
             return "I am fish";
         }
+
+        public override double GetPercentOfAmount()
+        {
+            Console.WriteLine("A: {0};\tT: {1}", amount, totalAmount);
+            return ((double)amount / (double)totalAmount) * 100;
+        }
     }
 
     class JellyFish : WaterAnimal
     {
-        private string _habitat;
+        private string habitat;
+
+        public string Habitat
+        {
+            get => habitat;
+        }
 
         public JellyFish()
         {
-            _habitat = "All oceans";
+            habitat = "All oceans";
         }
         public override string Voice()
         {
@@ -124,7 +153,29 @@
 
     class OverLand : Animals
     {
-        
+        private string habitat;
+        protected int amountOfOverlandAnimals;
+
+        public string Habitat
+        {
+            get => habitat;
+        }
+
+        public OverLand()
+        {
+            habitat = "All contitents";
+            amountOfOverlandAnimals = 500000000;
+        }
+        public override string Voice()
+        {
+            return "I am overland animal";
+        }
+
+        public double GetPercentOfAmount()
+        {
+            Console.WriteLine("A: {0};\tT: {1}", amountOfOverlandAnimals, totalAmount);
+            return ((double)amountOfOverlandAnimals / (double)totalAmount) * 100;
+        }
     }
 
     class Bird : OverLand
