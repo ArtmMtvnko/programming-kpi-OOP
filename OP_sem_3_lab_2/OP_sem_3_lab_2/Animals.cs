47b3d616cf709a1e84d940a0853e3cd041c30a59
Матвієнко Артем <artemm3notegp@gmail.com>
2023-10-27 15:46:32 +0300

    Copied class structure from Lab1 to Lab2. Added delegate and event. Conform event and new class Employee (to make long story short - complited 1,2,3,4.a,4.b tasks in lab 2)

---
+++ OP_sem_3_lab_2/OP_sem_3_lab_2/Animals.cs
@@ -0,0 +1,452 @@
+﻿using System;
+
+namespace OP_sem_3_lab_2
+{
+    interface IFlyable
+    {
+        void Fly();
+
+        void Fall();
+    }
+
+    interface IFalling
+    {
+        void Fall();
+    }
+
+    class MyEventArgs : EventArgs
+    {
+        public Animal Animal { get; }
+
+        public MyEventArgs(Animal animal)
+        {
+            Animal = animal;
+        }
+    }
+
+    class Employee
+    {
+        List<Animal> toDoList = new List<Animal>();
+
+        public string ToDoList
+        {
+            get
+            {
+                string output = "[ ";
+                toDoList.ForEach(animal =>
+                {
+                    output += $"{animal.GetTypeOfAnimal()}, ";
+                });
+                return output + "]";
+            }
+        }
+
+        public Employee(Zoo zoo)
+        {
+            zoo.AnimalAdded += AddTask; // += new AnimalAddHandler(AddTask);
+        }
+
+        public void AddTask(Zoo sender, MyEventArgs e)
+        {
+            toDoList.Add(e.Animal);
+            Console.WriteLine("In zoo came new animal. I have more work :(");
+        }
+    }
+
+    class Zoo
+    {
+        public delegate void AnimalAddHandler(Zoo sender, MyEventArgs e);
+
+        public event AnimalAddHandler AnimalAdded;
+
+        private static Zoo instance;
+        private Animal[] animals;
+        private int pointer = 0;
+
+        public Animal[] Animals { get => animals; }
+
+        private Zoo(int capacity)
+        {
+            animals = new Animal[capacity];
+        }
+
+        public static Zoo GetInstance(int capacity = 50)
+        {
+            if (instance == null)
+                instance = new Zoo(capacity);
+            return instance;
+        }
+
+        public void AddAnimal(Animal animal)
+        {
+            if (pointer == animals.Length) return; // throw MyExeption
+            animals[pointer++] = animal;
+
+            AnimalAdded?.Invoke(this, new MyEventArgs(animal));
+        }
+
+        public override string ToString()
+        {
+            string output = "[ ";
+
+            foreach (var animal in animals)
+            {
+                if (animal == null) break;
+                output += $"{animal.GetTypeOfAnimal()}, ";
+            }
+
+            return output + " ]";
+        }
+    }
+
+    abstract class Animal
+    {
+        private static int totalAmount;
+        private string habitat;
+
+        public static int TotalAmount { get => totalAmount; }
+
+        public string Habitat { get => habitat; }
+
+        static Animal()
+        {
+            totalAmount = WaterAnimal.TotalAmount + OverLand.TotalAmount;
+        }
+
+        public abstract string Voice();
+
+        public abstract string GetTypeOfAnimal();
+    }
+
+
+    class WaterAnimal : Animal
+    {
+        private static int totalAmount;
+        private string habitat;
+        private string type;
+
+        public static int TotalAmount { get => totalAmount; }
+
+        static WaterAnimal() => totalAmount = Dolphin.TotalAmount + JellyFish.TotalAmount;
+
+        public string Habitat
+        {
+            get => habitat;
+        }
+
+        public WaterAnimal()
+        {
+            habitat = "Water";
+            type = "Water Animal";
+        }
+
+        public override string Voice()
+        {
+            return "I am water animal";
+        }
+
+        public virtual double GetPercentOfAmount()
+        {
+            Console.WriteLine("A: {0};\tT: {1}", totalAmount, Animal.TotalAmount);
+            return ((double)totalAmount / (double)Animal.TotalAmount) * 100;
+        }
+
+        public override string GetTypeOfAnimal()
+        {
+            return type;
+        }
+    }
+
+    class Dolphin : WaterAnimal
+    {
+        private static int totalAmount;
+        private string habitat;
+        private string type;
+
+        public static int TotalAmount
+        {
+            get { return totalAmount; }
+            set
+            {
+                if (value >= 0)
+                {
+                    totalAmount = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+
+
+        public string Habitat
+        {
+            get => habitat;
+        }
+
+        static Dolphin() => totalAmount = 1300000000;
+
+        public Dolphin()
+        {
+            habitat = "All ponds";
+            type = "Dolphin";
+        }
+
+        public override string Voice()
+        {
+            return "*squeak*";
+        }
+
+        public override double GetPercentOfAmount()
+        {
+            Console.WriteLine("A: {0};\tT: {1}", totalAmount, Animal.TotalAmount);
+            return ((double)totalAmount / (double)Animal.TotalAmount) * 100;
+        }
+
+        public override string GetTypeOfAnimal()
+        {
+            return type;
+        }
+    }
+
+    class JellyFish : WaterAnimal
+    {
+        private static int totalAmount;
+        private string habitat;
+        private string type;
+
+        public static int TotalAmount
+        {
+            get { return totalAmount; }
+            set
+            {
+                if (value >= 0)
+                {
+                    totalAmount = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+
+        public string Habitat
+        {
+            get => habitat;
+        }
+
+        static JellyFish() => totalAmount = 200000000;
+
+        public JellyFish()
+        {
+            habitat = "All oceans";
+            type = "Jellyfish";
+        }
+        public override string Voice()
+        {
+            return "*mute*";
+        }
+
+        public override double GetPercentOfAmount()
+        {
+            Console.WriteLine("A: {0};\tT: {1}", totalAmount, Animal.TotalAmount);
+            return ((double)totalAmount / (double)Animal.TotalAmount) * 100;
+        }
+
+        public override string GetTypeOfAnimal()
+        {
+            return type;
+        }
+    }
+
+
+    class OverLand : Animal
+    {
+        private static int totalAmount;
+        private string habitat;
+        private string type;
+
+        public static int TotalAmount { get => totalAmount; }
+
+        public string Habitat
+        {
+            get => habitat;
+        }
+
+        static OverLand() => totalAmount = Parrot.TotalAmount + Primate.TotalAmount;
+
+        public OverLand()
+        {
+            habitat = "Land";
+            type = "Overland Animal";
+        }
+        public override string Voice()
+        {
+            return "I am overland animal";
+        }
+
+        public virtual double GetPercentOfAmount()
+        {
+            Console.WriteLine("A: {0};\tT: {1}", totalAmount, Animal.TotalAmount);
+            return ((double)totalAmount / (double)Animal.TotalAmount) * 100;
+        }
+
+        public override string GetTypeOfAnimal()
+        {
+            return type;
+        }
+    }
+
+    class Parrot : OverLand, IDisposable, IFlyable, IFalling
+    {
+        private static int totalAmount;
+        private string habitat;
+        private string type;
+        private bool disposed = false;
+
+        public static int TotalAmount
+        {
+            get { return totalAmount; }
+            set
+            {
+                if (value >= 0)
+                {
+                    totalAmount = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+
+        public string Habitat
+        {
+            get => habitat;
+        }
+
+        static Parrot() => totalAmount = 100000000;
+
+        public Parrot()
+        {
+            habitat = "All continents";
+            type = "Parrot";
+        }
+        public override string Voice()
+        {
+            return "Ahrrr";
+        }
+
+        public override double GetPercentOfAmount()
+        {
+            Console.WriteLine("A: {0};\tT: {1}", totalAmount, Animal.TotalAmount);
+            return ((double)totalAmount / (double)Animal.TotalAmount) * 100;
+        }
+
+        public override string GetTypeOfAnimal() => type;
+
+        public void Fly()
+        {
+            Console.WriteLine("I am fling...");
+        }
+
+        public void Fall()
+        {
+            Console.WriteLine("I am falling...");
+        }
+
+        void IFlyable.Fall()
+        {
+            Console.WriteLine("This Fall() from IFlyable");
+        }
+
+        void IFalling.Fall()
+        {
+            Console.WriteLine("This Fall() from IFalling");
+        }
+
+        public void Dispose()
+        {
+            CleanUp(true);
+
+            GC.SuppressFinalize(this);
+        }
+
+        private void CleanUp(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    Console.WriteLine("I am from dispose");
+                }
+                Console.WriteLine("Logic from destructor in CleanUp");
+
+                // this.disposed = true;
+                // Don't know proper way, this.disposed = true; -- inside "if" block
+                // or outside like below?
+            }
+            this.disposed = true;
+        }
+
+        ~Parrot()
+        {
+            Console.WriteLine("I am from destructor (finalize)");
+            CleanUp(false);
+        }
+    }
+
+    class Primate : OverLand
+    {
+        private static int totalAmount;
+        private string habitat;
+        private string type;
+        byte[] a = new byte[1024 * 1000];
+
+        public static int TotalAmount
+        {
+            get { return totalAmount; }
+            set
+            {
+                if (value >= 0)
+                {
+                    totalAmount = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+
+        public string Habitat
+        {
+            get => habitat;
+        }
+
+        static Primate() => totalAmount = 400000000;
+
+        public Primate()
+        {
+            //Console.WriteLine("I have borned 0_0");
+            habitat = "All continents";
+            type = "Primate";
+        }
+
+        ~Primate() { Console.WriteLine("I am done...... x_x"); }
+        public override string Voice()
+        {
+            return "U-u-u-a-a-a";
+        }
+
+        public override double GetPercentOfAmount()
+        {
+            Console.WriteLine("A: {0};\tT: {1}", totalAmount, Animal.TotalAmount);
+            return ((double)totalAmount / (double)Animal.TotalAmount) * 100;
+        }
+
+        public override string GetTypeOfAnimal() => type;
+    }
+}
--- OP_sem_3_lab_2/OP_sem_3_lab_2/Program.cs
+++ OP_sem_3_lab_2/OP_sem_3_lab_2/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //MyMethod <=> = new Test(MyMethod)
+            //test() <=> test.Invoke()
+
+            IFlyable parrot = new Parrot();
+            parrot.Fall();
+
+            IFalling parrot2 = new Parrot();
+            parrot2.Fall();
+
+            Zoo zoo = Zoo.GetInstance(10);
+            Employee employee = new Employee(zoo);
+
+            zoo.AddAnimal(new Parrot());
+            zoo.AddAnimal(new Primate());
+            zoo.AddAnimal(new Dolphin());
+
+            Console.WriteLine(employee.ToDoList);
         }
+
     }
 }
