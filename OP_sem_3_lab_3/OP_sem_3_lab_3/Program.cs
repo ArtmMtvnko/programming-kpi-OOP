f50ee86c282ce2903f89e75998cf03f2235b1831
Матвієнко Артем <artemm3notegp@gmail.com>
2023-11-10 15:08:47 +0200

    Created lab_3 project. Added extra files/folders in gitignore

---
+++ OP_sem_3_lab_3/OP_sem_3_lab_3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OP_sem_3_lab_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
